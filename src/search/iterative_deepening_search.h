df40a55da704da09a12f85aa552e23c49fb11fac
Thomas Keller <16223802+thomaskeller79@users.noreply.github.com>
2015-12-11 11:37:02 +0100

    Removed MLS fallback option from IDS.

--- src/search/iterative_deepening_search.h
+++ src/search/iterative_deepening_search.h
@@ -2,8 +2,8 @@
 #define ITERATIVE_DEEPENING_SEARCH_H
 
 // Implements an iterative deepending search engine. This was used as
-// initialization for UCT in IPPC 2011 and is described in the paper
-// by Keller and Eyerich (2012).
+// initialization for UCT in IPPC 2011 (and IPPC 2014) and is described in the
+// paper by Keller and Eyerich (ICAPS 2012).
 
 #include "search_engine.h"
 #include "states.h"
@@ -13,7 +13,6 @@
 #include <unordered_map>
 
 class DepthFirstSearch;
-class MinimalLookaheadSearch;
 
 class IDS : public DeterministicSearchEngine {
 public:
@@ -40,17 +39,16 @@ public:
                          std::vector<double>& qValues) override;
 
     // Parameter setter
-    void setMaxSearchDepth(int _maxSearchDepth);
+    void setMaxSearchDepth(int _maxSearchDepth) override;
+    void setCachingEnabled(bool newValue) override;
 
-    virtual void setStrictTerminationTimeout(double newValue) {
+    void setStrictTerminationTimeout(double newValue) {
         strictTerminationTimeout = newValue;
     }
 
-    virtual void setTerminateWithReasonableAction(bool newValue) {
+    void setTerminateWithReasonableAction(bool newValue) {
         terminateWithReasonableAction = newValue;
     }
-
-    virtual void setCachingEnabled(bool newValue);
 
     // Reset statistic variables
     void resetStats();
@@ -67,12 +65,13 @@ public:
 
 protected:
     // Decides whether more iterations are possible and reasonable
-    bool moreIterations(std::vector<int> const& actionsToExpand,
+    bool moreIterations(int const& stepsToGo,
+                        std::vector<int> const& actionsToExpand,
                         std::vector<double>& qValues);
-    inline bool moreIterations();
+    inline bool moreIterations(int const& stepsToGo);
 
-    // The state that is given iteratively to the DFS engine
-    State currentState;
+    // The depth first search engine
+    DepthFirstSearch* dfs;
 
     // Learning related variables
     bool isLearning;
@@ -81,15 +80,6 @@ protected:
     // The timer that is used to make sure that computation doesn't take too
     // much time
     Timer timer;
-
-    // The depth first search engine
-    DepthFirstSearch* dfs;
-
-    // The minimal lookahead search engine that is used if initialization with
-    // this is too costly. TODO: In the future, we should change bool
-    // estimateBestAction to SearchEngine* estimateBestAction and return another
-    // search engine if this wants to be replaced
-    MinimalLookaheadSearch* mls;
 
     // The number of remaining steps for this step
     int maxSearchDepthForThisStep;
