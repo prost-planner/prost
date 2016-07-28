f5bf2a5992e445857aa61cdc371bb6b27829e9f4
?or?e Reli? <dorde.relic@unibas.ch>
2016-07-28 22:01:17 +0200

    fix: Moved map for storing pVarDeclaration from parser.ypp to rddl.cc

--- src/rddl_parser/rddl.h
+++ src/rddl_parser/rddl.h
@@ -15,8 +15,6 @@ class Object;
 class Type;
 class ParametrizedVariable;
 
-extern std::map<std::string, PvarDefinition*> parametrizedVariableDefinitionsMap; // Map for storing definition of ParametrizedVariables
-extern std::map<std::string, PvarExpression*> parametrizedVariableMap; // Map for storing ParametrizedVariables as expressions
 extern std::map<std::string, Object*> objectMap; // Map for storing defined objects
 extern std::map<std::string, Type*> typeMap; // Map for storing defined types
 
