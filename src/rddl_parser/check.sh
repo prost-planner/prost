9465dd2d60202d8a222313ac4cd19efdc4fb3d3e
?or?e Reli? <dorde.relic@unibas.ch>
2016-07-29 11:53:37 +0200

    Added shell script for quick check of the results (should work on any PC).

---
+++ src/rddl_parser/check.sh
@@ -0,0 +1,5 @@
+./rddl-parser ../../testbed/benchmarks/ippc-all/rddl/elevators_mdp.rddl ../../testbed/benchmarks/ippc-all/rddl/elevators_inst_mdp__2.rddl ~/Desktop
+meld ~/Desktop/elevators_inst_mdp__2 ~/projects/prost/testbed/benchmarks/ippc-all/prost/elevators_inst_mdp__2
+
+./rddl-parser ../../testbed/benchmarks/ippc-all/rddl/crossing_traffic_mdp.rddl ../../testbed/benchmarks/ippc-all/rddl/crossing_traffic_inst_mdp__2.rddl ~/Desktop
+meld ~/Desktop/elevators_inst_mdp__2 ~/projects/prost/testbed/benchmarks/ippc-all/prost/elevators_inst_mdp__2
