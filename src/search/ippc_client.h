82d06f06532c0e2a9f0099fe2f24dd765e5b4d3e
Florian Geisser <17546708+geisserf@users.noreply.github.com>
2018-01-17 14:54:12 +0100

    Added functionality to receive task description from server instead from a preparsed file.

--- src/search/ippc_client.h
+++ src/search/ippc_client.h
@@ -1,36 +1,32 @@
 #ifndef RDDL_CLIENT_H
 #define RDDL_CLIENT_H
 
+#include "prost_planner.h"
+
 #include <map>
+#include <memory>
 #include <string>
 #include <vector>
 
-class ProstPlanner;
 class XMLNode;
 
 class IPPCClient {
 public:
-    IPPCClient(
-        ProstPlanner* _planner, std::string _hostName, unsigned short _port,
-        std::map<std::string, int> const& _stateVariableIndices,
-        std::vector<std::vector<std::string>> const& _stateVariableValues)
-        : planner(_planner),
-          hostName(_hostName),
+    IPPCClient(std::string _hostName, unsigned short _port)
+        : hostName(_hostName),
           port(_port),
           socket(-1),
           numberOfRounds(-1),
-          remainingTime(0),
-          stateVariableIndices(_stateVariableIndices),
-          stateVariableValues(_stateVariableValues) {}
+          remainingTime(0) {}
 
-    void run(std::string const& problemName);
+    void run(std::string const& problem, std::string& plannerDesc);
 
 private:
     void initConnection();
     int connectToServer();
     void closeConnection();
 
-    void initSession(std::string const& rddlProblem);
+    void initSession(std::string const& rddlProblem, std::string& plannerDesc);
     void finishSession();
 
     void initRound(std::vector<double>& initialState, double& immediateReward);
@@ -44,7 +40,15 @@ private:
     void readVariable(XMLNode const* node,
                       std::map<std::string, std::string>& result);
 
-    ProstPlanner* planner;
+    // Writes domain and instance from task description to temporary files.
+    void generateTempFiles(std::string const& taskDesc) const;
+    void removeTempFiles() const;
+    // If the client call did not contain a task file, we have to read the task
+    // description from the server and run the external parser to create a task
+    // in PROST format.
+    void executeParser(std::string const& taskDesc);
+
+    std::unique_ptr<ProstPlanner> planner;
     std::string hostName;
     unsigned short port;
     int socket;
