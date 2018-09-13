df2b0b3b5d776b64f68f8f5774c14581745cd679
Florian Geisser <17546708+geisserf@users.noreply.github.com>
2018-09-13 15:56:08 +0200

    Changed isDirectory implementation to use std::filesystem.

--- src/rddl_parser/utils/system_utils.cc
+++ src/rddl_parser/utils/system_utils.cc
@@ -4,6 +4,7 @@
 #include <cassert>
 #include <cstdlib>
 #include <ctime>
+#include <experimental/filesystem>
 #include <fstream>
 #include <iostream>
 #include <sstream>
@@ -11,11 +12,11 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "string.h"
-#include <sys/stat.h>
 #include "sys/sysinfo.h"
 #include "sys/times.h"
 #include "sys/types.h"
 #include "sys/vtimes.h"
+#include <sys/stat.h>
 
 clock_t SystemUtils::start = 0;
 clock_t SystemUtils::lastCPU = 0;
@@ -24,6 +25,8 @@ clock_t SystemUtils::lastUserCPU = 0;
 int SystemUtils::numProcessors = 0;
 bool SystemUtils::clockRunning = false;
 bool SystemUtils::CPUMeasurementOfProcessRunning = false;
+
+namespace fs = std::experimental::filesystem;
 
 void SystemUtils::abort(std::string msg) {
     std::cerr << "\033[1;31mERROR! \033[0m" << msg << std::endl;
@@ -51,9 +54,7 @@ double SystemUtils::stopTime() {
 }
 
 bool SystemUtils::isDirectory(std::string const& name) {
-    struct stat buf;
-    stat(name.c_str(), &buf);
-    return S_ISDIR(buf.st_mode);
+    return fs::is_directory(name);
 }
 
 bool SystemUtils::readFile(std::string& file, std::string& res,
