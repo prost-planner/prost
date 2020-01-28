d25146b9dfbedf38fe7d23dcda36816c08a789a4
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-01-28 10:46:03 +0100

    use argparse in run-server.py

--- testbed/run-server.py
+++ testbed/run-server.py
@@ -1,9 +1,39 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import subprocess
 import sys
 import os
+
+def parse_arguments():
+    formatter = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,max_help_position=48)
+    parser = argparse.ArgumentParser(description = 'Run RDDL server from directory defined '
+                                                   'by environment variable RDDLSIM_ROOT.',
+                                     formatter_class = formatter)
+    parser.add_argument('benchmark_dir', action='store',
+                        help='Path to RDDL files.')
+    parser.add_argument('port', action='store', default = '2323', nargs='?',
+                        help='Port number where the server listens.')
+    parser.add_argument('num_rounds', action='store', default = '30', nargs='?',
+                        help='Number of rounds.')
+    parser.add_argument('seed', action='store', default = '0', nargs='?',
+                        help='Random seed of the server.')
+    parser.add_argument('kill_after_first_session', action='store', default = '0', nargs='?',
+                        help='If this is 1, the server is killed after one interaction with a client. '
+                        'Otherwise, it waits for the next client to connect.')
+    parser.add_argument('timeout', action='store', default = '0', nargs='?',
+                        help='Timeout in seconds. If "0" is passed then no timeout is used.')
+    parser.add_argument('log_dir', action='store', default = '/logs', nargs='?',
+                        help='Directory where log files are written.')
+    parser.add_argument('monitor_execution', action='store', default = '0', nargs='?',
+                        help='If this is 1, the client must specify that it wishes that a round "counts".')
+    parser.add_argument('initial_memory_alloc', action='store', default = '100M', nargs='?',
+                        help='Initial amount of memory allocated by the Java VM.')
+    parser.add_argument('maximum_memory_alloc', action='store', default = '500M', nargs='?',
+                        help='Maximum amount of memory that can be allocated by the Java VM.')
+    args = parser.parse_args()
+    return args
 
 if __name__== "__main__":
     # Check if the environment variable "RDDLSIM_ROOT" exists
@@ -15,69 +45,16 @@ if __name__== "__main__":
         print(err_msg)
         exit(1)
 
-        
-    # The only mandatory parameter of the script is the folder containing
-    # the instances
-    if len(sys.argv) < 2:
-        err_msg = 'Error: specify where the RDDL files can be found'
-        print(err_msg)
-        exit(1)
-    benchmark_dir = sys.argv[1]
-
-    # The port where the server listens
-    port = '2323'
-    if len(sys.argv) > 2:
-        port = sys.argv[2]
-
-    num_rounds = '30'
-    if len(sys.argv) > 3:
-        num_rounds = sys.argv[3]
-
-    # The random seed of the server
-    seed = '0'
-    if len(sys.argv) > 4:
-        seed = sys.argv[4]
-
-    # If this is 1, the server is killed after one interaction with a client.
-    # Otherwise, it waits for the next client to connect.
-    kill_after_first_session = '0'
-    if len(sys.argv) > 5:
-        kill_after_first_session = sys.argv[5]
-
-    # The total timeout for the instance in seconds. If this is 0, no timeout
-    # at all is used.
-    timeout = '0'
-    if len(sys.argv) > 6:
-        timeout = sys.argv[6]
-
-    # The directory where the server log files are written
-    log_dir = '/logs'
-    if len(sys.argv) > 7:
-        log_dir = sys.argv[7]
-
-    # If this is 1, the client must specify that it wishes that a round "counts"
-    monitor_execution = '0'
-    if len(sys.argv) > 8:
-        monitor_execution = sys.argv[8]
-
-    # Specify the memory that is initially allocated by the Java VM
-    initial_memory_alloc = "100M"
-    if len(sys.argv) > 9:
-        initial_memory_alloc = sys.argv[9]
-
-    # Specify the maximum memory that may be allocated by the Java VM
-    maximum_memory_alloc = "500M"
-    if len(sys.argv) > 10:
-        maximum_memory_alloc = sys.argv[10]
+    args = parse_arguments()
 
     lib_dir = os.path.join(rddlsim_root, 'lib')
     bin_dir = os.path.join(rddlsim_root, 'bin')
     jars = os.listdir(lib_dir)
     full_jars = [os.path.join(lib_dir, jar) for jar in jars]
 
-    subprocess.check_call(["java", "-Xms{}".format(initial_memory_alloc),
-                           "-Xmx{}".format(maximum_memory_alloc), "-classpath",
+    subprocess.check_call(["java", "-Xms{}".format(args.initial_memory_alloc),
+                           "-Xmx{}".format(args.maximum_memory_alloc), "-classpath",
                            "{}:{}".format(bin_dir, ":".join(full_jars)),
-                           "rddl.competition.Server", benchmark_dir, port,
-                           num_rounds, seed, kill_after_first_session, timeout,
-                           log_dir, monitor_execution])
+                           "rddl.competition.Server", args.benchmark_dir, args.port,
+                           args.num_rounds, args.seed, args.kill_after_first_session, args.timeout,
+                           args.log_dir, args.monitor_execution])
