88c730e93b0d6cc750361daca82fae6903013013
Augusto B. Corrêa <augusto.blaascorrea@unibas.ch>
2020-01-28 13:05:41 +0100

    add option to run all benchmarks to run-server.py

--- testbed/run-server.py
+++ testbed/run-server.py
@@ -3,34 +3,37 @@
 
 import argparse
 import subprocess
+import shutil
 import sys
 import os
 
 def parse_arguments():
-    formatter = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,max_help_position=48)
+    formatter = lambda prog: argparse.ArgumentDefaultsHelpFormatter(prog,max_help_position=32)
     parser = argparse.ArgumentParser(description = 'Run RDDL server from directory defined '
                                                    'by environment variable RDDLSIM_ROOT.',
                                      formatter_class = formatter)
-    parser.add_argument('benchmark_dir', action='store',
+    parser.add_argument('--all-benchmarks', action='store_true', required=False,
+                        help="Run all benchmarks.")
+    parser.add_argument('-b', '--benchmark', action='store', default=None,
                         help='Path to RDDL files.')
-    parser.add_argument('port', action='store', default = '2323', nargs='?',
+    parser.add_argument('-p', '--port', action='store', default = '2323', type=str,
                         help='Port number where the server listens.')
-    parser.add_argument('num_rounds', action='store', default = '30', nargs='?',
+    parser.add_argument('-r', '--num-rounds', action='store', default = '30', type=str,
                         help='Number of rounds.')
-    parser.add_argument('seed', action='store', default = '0', nargs='?',
+    parser.add_argument('-s', '--seed', action='store', default = '0', type=str,
                         help='Random seed of the server.')
-    parser.add_argument('kill_after_first_session', action='store', default = '0', nargs='?',
+    parser.add_argument('-k', '--kill-after-first-session', action='store', default = '0', type=str,
                         help='If this is 1, the server is killed after one interaction with a client. '
                         'Otherwise, it waits for the next client to connect.')
-    parser.add_argument('timeout', action='store', default = '0', nargs='?',
+    parser.add_argument('-t', '--timeout', action='store', default = '5', type=str,
                         help='Timeout in seconds. If "0" is passed then no timeout is used.')
-    parser.add_argument('log_dir', action='store', default = '/logs', nargs='?',
+    parser.add_argument('-l', '--log-dir', action='store', default = '/logs',
                         help='Directory where log files are written.')
-    parser.add_argument('monitor_execution', action='store', default = '0', nargs='?',
+    parser.add_argument('-e', '--monitor-execution', action='store', default = '0', type=str,
                         help='If this is 1, the client must specify that it wishes that a round "counts".')
-    parser.add_argument('initial_memory_alloc', action='store', default = '100M', nargs='?',
+    parser.add_argument('-m', '--initial-memory-alloc', action='store', default = '100M',
                         help='Initial amount of memory allocated by the Java VM.')
-    parser.add_argument('maximum_memory_alloc', action='store', default = '500M', nargs='?',
+    parser.add_argument('-M', '--maximum-memory-alloc', action='store', default = '500M',
                         help='Maximum amount of memory that can be allocated by the Java VM.')
     args = parser.parse_args()
     return args
@@ -52,9 +55,44 @@ if __name__== "__main__":
     jars = os.listdir(lib_dir)
     full_jars = [os.path.join(lib_dir, jar) for jar in jars]
 
-    subprocess.check_call(["java", "-Xms{}".format(args.initial_memory_alloc),
-                           "-Xmx{}".format(args.maximum_memory_alloc), "-classpath",
-                           "{}:{}".format(bin_dir, ":".join(full_jars)),
-                           "rddl.competition.Server", args.benchmark_dir, args.port,
-                           args.num_rounds, args.seed, args.kill_after_first_session, args.timeout,
-                           args.log_dir, args.monitor_execution])
+    if args.all_benchmarks:
+        print ('Running all benchmarks...')
+        if args.benchmark is not None:
+            print ('The "benchmark" argument will be ignored!')
+
+        if not os.path.exists('tmp-benchmark'):
+            os.mkdir('tmp-benchmark')
+
+        for subdir, dirs, files in os.walk('benchmarks/'):
+            for file in files:
+                src_file = '{}/{}/{}'.format(os.getcwd(), subdir, file)
+                year = subdir.split('-')[-1]
+                symlink_name = 'tmp-benchmark/' + file.replace('.rddl', '_'+year+'.rddl')
+                try:
+                    os.symlink(src_file,  symlink_name)
+                except OSError:
+                    print ("Error creating symlink of file {0} to {1}. {1} probably already exists.".format(src_file, symlink_name))
+
+        subprocess.check_call(["java", "-Xms{}".format(args.initial_memory_alloc),
+                               "-Xmx{}".format(args.maximum_memory_alloc), "-classpath",
+                               "{}:{}".format(bin_dir, ":".join(full_jars)),
+                               "rddl.competition.Server", 'tmp-benchmark/', args.port,
+                               args.num_rounds, args.seed, args.kill_after_first_session, args.timeout,
+                               args.log_dir, args.monitor_execution])
+
+
+        # Delete temporary directory
+        shitul.rmtree('tmp-benchmark')
+        os.rmdir('tmp-benchmark')
+
+    else:
+        if args.benchmark == None:
+            print ('No benchmark specified.')
+            sys.exit()
+        print ('Running benchmark %s...' % args.benchmark)
+        subprocess.check_call(["java", "-Xms{}".format(args.initial_memory_alloc),
+                               "-Xmx{}".format(args.maximum_memory_alloc), "-classpath",
+                               "{}:{}".format(bin_dir, ":".join(full_jars)),
+                               "rddl.competition.Server", args.benchmark, args.port,
+                               args.num_rounds, args.seed, args.kill_after_first_session, args.timeout,
+                               args.log_dir, args.monitor_execution])
