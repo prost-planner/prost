d2843681c65825a21bf528f8cf1d17fd73a49b97
Thomas Keller <tho.keller@unibas.ch>
2020-04-08 23:21:43 +0200

    Issue-83: Remove files that were moved to prostlab

--- testbed/run-experiment.py
+++ testbed/run-experiment.py
@@ -4,30 +4,31 @@
 import platform
 
 from lab.environments import LocalEnvironment, BaselSlurmEnvironment
+from prostlab.experiment import ProstExperiment
 
 from benchmark_suites import *
-from experiment import ProstExperiment
-
 
 def is_running_on_cluster():
     node = platform.node()
     return node.endswith(".scicore.unibas.ch") or node.endswith(".cluster.bc2.ch")
 
-
 REPO = "/home/kellert/workspace/planner/prost/prost-dev/"
-REV = "9758a9bb"
-ENV = LocalEnvironment(processes=4)
-SUITES = [ELEVATORS_2011[0]]
-NUM_RUNS = 1
+REV = "4b7582c1"
+
+TIME_PER_STEP = 1.0
 if is_running_on_cluster():
     ENV = BaselSlurmEnvironment(partition="infai_2", email="tho.keller@unibas.ch")
     SUITES = IPC_ALL
     NUM_RUNS = 30
+    
+else:
+    ENV = LocalEnvironment(processes=4)
+    SUITES = [ELEVATORS_2011[0]]
+    NUM_RUNS = 1
 
-exp = ProstExperiment(suites=SUITES, environment=ENV, num_runs=NUM_RUNS)
+exp = ProstExperiment(suites=SUITES, environment=ENV, num_runs=NUM_RUNS, time_per_step=TIME_PER_STEP)
 
-if is_running_on_cluster():
-    exp.add_config("ipc11", REPO, REV, "IPC2011", build_options=["-j6"])
+exp.add_config("ipc11", REPO, REV, "IPC2011", build_options=["-j6"])
 exp.add_config("ipc14", REPO, REV, "IPC2014", build_options=["-j6"])
 
 exp.add_step("build", exp.build)
