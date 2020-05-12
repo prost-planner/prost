482bf17cf4737b16c4b45b7b04c101d5352aef20
Thomas Keller <tho.keller@unibas.ch>
2020-05-12 12:20:15 +0200

    Another change to prostlab-wrapper

--- testbed/prostlab-wrapper.sh
+++ testbed/prostlab-wrapper.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$1 -b $2 -p $3 -s $4 --separate-session -t $5 -l ./ -r $6 > ./server.log 2> ./server.err & sleep 10 && $7 $8 -p $3 --parser-options "-ipc2018 $9" [Prost ${10} -se [${11}]]
+$1 -b $2 -p $3 -s $4 --separate-session -t $5 -l ./ -r $6 > ./server.log 2> ./server.err & sleep 10 && $7 $8 -p $3 --parser-options $9 [Prost ${10} -se [${11}]]
