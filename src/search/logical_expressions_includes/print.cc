a4f67da4b62708627a02e66cf0f6f3af1af6a48c
Florian Geisser <florian.geisser@anu.edu.au>
2021-02-08 12:27:07 +1100

    Issue 132: fixed wrong cast in EqualsExpression::print.

--- src/search/logical_expressions_includes/print.cc
+++ src/search/logical_expressions_includes/print.cc
@@ -38,8 +38,8 @@ void Disjunction::print(ostream& out) const {
 
 void EqualsExpression::print(ostream& out) const {
     if ((exprs.size() == 2)) {
-        ActionFluent* af = static_cast<ActionFluent*>(exprs[0]);
-        NumericConstant* val = static_cast<NumericConstant*>(exprs[1]);
+        ActionFluent* af = dynamic_cast<ActionFluent*>(exprs[0]);
+        NumericConstant* val = dynamic_cast<NumericConstant*>(exprs[1]);
         if (af && val) {
             out << af->values[val->value];
             return;
